e5a7c56f52fd3f1b4fb9aef8962f7d151d31ed83
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-11-24 19:41:36 +0100

    Update agePyramid.js

--- 2025/stats/js/agePyramid.js
+++ 2025/stats/js/agePyramid.js
@@ -8,7 +8,7 @@ export async function renderAgePyramid(sectionName, canvasId, containerId, title
 
   const rows = group.length;
   const rowHeight = 18;
-  const topBottomPadding = 120;
+  const topBottomPadding = 150;
   const barThicknessPixel = 18;
 
   // Dynamische Höhe
