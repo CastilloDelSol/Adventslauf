71ca72c2f790a13bcbdec465b7db3d6d3b3dc418
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-11-24 13:06:13 +0100

    Update donutGender.js

--- 2025/stats/js/donutGender.js
+++ 2025/stats/js/donutGender.js
@@ -1,5 +1,22 @@
 import { loadAgeData } from "./dataLoader.js";
 
+const centerTextPlugin = {
+  id: 'centerText',
+  afterDraw(chart) {
+    const {ctx, chartArea: {width, height}} = chart;
+    const dataset = chart.data.datasets[0].data;
+    const total = dataset[0] + dataset[1];
+
+    ctx.save();
+    ctx.font = "bold 22px Arial";
+    ctx.fillStyle = "#333";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(total, width / 2, height / 2);
+    ctx.restore();
+  }
+};
+
 export async function renderGenderDonut(sectionName, canvasId) {
     const data = await loadAgeData();
 
@@ -22,7 +39,7 @@ export async function renderGenderDonut(sectionName, canvasId) {
             totalW += e.W;
         });
     }
-
+    
     new Chart(document.getElementById(canvasId), {
         type: "doughnut",
         data: {
@@ -34,6 +51,8 @@ export async function renderGenderDonut(sectionName, canvasId) {
         },
         options: {
             responsive: true
-        }
+        },
+        plugins: [centerTextPlugin]
     });
+
 }
