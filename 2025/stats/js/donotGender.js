4cde62ee329aee34c9c064871b2c2c2eb2da605e
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-11-24 09:33:44 +0100

    Create donotGender.js

---
+++ 2025/stats/js/donotGender.js
@@ -0,0 +1,27 @@
+import { loadAgeData, getSection } from "./dataLoader.js";
+
+export async function renderGenderDonut(sectionName, canvasId) {
+    await loadAgeData();
+
+    const group = getSection(sectionName);
+
+    let totalM = 0;
+    let totalW = 0;
+
+    group.forEach(e => {
+        totalM += e.M;
+        totalW += e.W;
+    });
+
+    new Chart(document.getElementById(canvasId), {
+        type: "doughnut",
+        data: {
+            labels: ["Männlich", "Weiblich"],
+            datasets: [{
+                data: [totalM, totalW],
+                backgroundColor: ["#4EA5E9", "#FF6384"]
+            }]
+        },
+        options: { responsive: true }
+    });
+}
