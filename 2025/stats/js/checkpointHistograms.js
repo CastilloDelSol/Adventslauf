e4f2766770f455abf756eaf6b3a99cc03d695ddd
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-12-11 20:00:47 +0100

    Update checkpointHistograms.js

--- 2025/stats/js/checkpointHistograms.js
+++ 2025/stats/js/checkpointHistograms.js
@@ -2,25 +2,6 @@
 import { gaussianKernel, computeKDE } from "./kde.js";
 import { loadCheckpointData, getCheckpointData } from "./dataLoader.js";
 
-
-/*
-function gaussianKernel(x) {
-    return Math.exp(-0.5 * x * x) / Math.sqrt(2 * Math.PI);
-}
-
-function computeKDE(xs, counts, bandwidth = 45) {
-    const kdeValues = xs.map(x0 => {
-        let sum = 0;
-        for (let i = 0; i < xs.length; i++) {
-            const u = (x0 - xs[i]) / bandwidth;
-            sum += counts[i] * gaussianKernel(u);
-        }
-        return sum;
-    });
-    return kdeValues;
-}
-*/
-
 /* -------------------------------------------------------
    Time formatters
 ------------------------------------------------------- */
